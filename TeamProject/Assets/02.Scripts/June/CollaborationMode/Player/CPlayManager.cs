267cd5f08a48f9feca95c2804f5b63f248ca8ede
June060522 <a80336234@gmail.com>
2022-11-27 18:39:55 +0900

    collaboration

---
+++ TeamProject/Assets/02.Scripts/June/CollaborationMode/Player/CPlayManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPlayManager : MonoBehaviour
+{
+    [SerializeField] protected GameObject throwFood;
+
+    [SerializeField] float hp;
+    public float Hp{get => hp; set => hp = value;}
+    [SerializeField] float speed;
+    public float Speed{get => speed; set => speed = value;}
+    [SerializeField] float jumpPower;
+    public float JumpPower{get => jumpPower; set => jumpPower = value;}
+
+    public bool isAlive = true;
+
+    protected Rigidbody2D rb2D;
+
+    private void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+}
